e15cdfeb6b5aec63575809be708abf0ffb6c2069
Joakim Wånggren <joakim.wanggren@schibsted.se>
2014-10-05 01:37:53 +0200

    Inital commit with working app

---
+++ app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var request = require('request'),
+    es = require('event-stream'),
+    config = require('./config.js');
+
+//request.debug = true;
+var baseRequest = request.defaults({
+  headers: {
+    'X-Api-Key': config.key,
+    'X-Api-Secret': config.secret
+  }
+});
+
+process.stdin
+  .pipe(es.split())
+  .pipe(es.map(function(num, cb) {
+    if(!parseInt(num, 10)) cb();
+
+    var url = '{b}/spaces/{s}/tickets/{#}.json'.replace('{b}', config.url).replace('{s}', config.space).replace('{#}', num);
+    baseRequest.get(url, function(err, res, body) {
+      if(err) return cb(err);
+      if(res.statusCode != 200) return cb(null, 'Ticket {n} not found'.replace('{n}', num));
+      var ticket = JSON.parse(body);
+      var out = ticket.status + '\t #'+ticket.number+' - '+ticket.summary;
+      return cb(null, out);
+    });
+  }))
+  .pipe(es.join('\n'))
+  .pipe(process.stdout);
---
+++ config-dist.js
@@ -0,0 +1,6 @@
+module.exports = {
+  key: 'api-key',
+  secret: 'api-secret',
+  space: 'assembla-space',
+  url: 'https://api.assembla.com/v1'
+};
