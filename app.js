3d7ea81860cb736dcd9f56d8329ee1fef227d510
Joakim Wånggren <joakim.wanggren@schibsted.se>
2014-10-05 12:38:40 +0200

    Added jshint and used jsFormatter

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 'use strict';
 
 var request = require('request'),
-    es = require('event-stream'),
-    config = require('./config.js');
+  es = require('event-stream'),
+  config = require('./config.js');
 
 //request.debug = true;
 var baseRequest = request.defaults({
@@ -17,14 +17,19 @@ var baseRequest = request.defaults({
 process.stdin
   .pipe(es.split())
   .pipe(es.map(function(num, cb) {
-    if(!parseInt(num, 10)) return cb();
-
+    if (!parseInt(num, 10)) {
+      return cb();
+    }
     var url = '{b}/spaces/{s}/tickets/{#}.json'.replace('{b}', config.url).replace('{s}', config.space).replace('{#}', num);
     baseRequest.get(url, function(err, res, body) {
-      if(err) return cb(err);
-      if(res.statusCode != 200) return cb(null, 'Ticket {n} not found'.replace('{n}', num));
+      if (err) {
+        return cb(err);
+      }
+      if (res.statusCode !== 200) {
+        return cb(null, 'Ticket {n} not found'.replace('{n}', num));
+      }
       var ticket = JSON.parse(body);
-      var out = ticket.status + '\t #'+ticket.number+' - '+ticket.summary;
+      var out = String.prototype.concat.call(ticket.status, '\t #', ticket.number, ' - ', ticket.summary);
       return cb(null, out);
     });
   }))
